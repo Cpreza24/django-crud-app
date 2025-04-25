7e69896832a9381114e0854d40920a115906d639
Cesar Preza <cpreza93@yahoo.com>
2025-04-24 19:49:41 -0700

    adds candy index

--- djangoapp/my_app/urls.py
+++ djangoapp/my_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path('candies/', views.candies_index, name='candies_index'),
+    path('candies/<int:candy_id>/', views.candies_detail, name='candies_detail'),
 ]
--- djangoapp/my_app/views.py
+++ djangoapp/my_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Candy
 
 # Create your views here.
 def home(request):
@@ -7,3 +8,12 @@ def home(request):
 
 def about(request):
     return HttpResponse('about page')
+
+def candies_index(request):
+    candies = Candy.objects.all()
+    return render(request, 'my_app/candies_index.html', {'candies': candies})
+
+def candies_detail(request, candy_id):
+    candy = Candy.objects.get(id=candy_id)
+    return render(request, 'my_app/candies_detail.html', {'candy': candy})
+
