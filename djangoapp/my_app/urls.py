5c69cfcc760d225e1894b4ba786eb7b2d34c959a
Cesar Preza <cpreza93@yahoo.com>
2025-04-24 19:58:21 -0700

    adds candy detail page

--- djangoapp/my_app/urls.py
+++ djangoapp/my_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('candies/', views.candies_index, name='candies_index'),
+    path('candies/create/', views.candies_create, name='candies_create'),
     path('candies/<int:candy_id>/', views.candies_detail, name='candies_detail'),
 ]
--- djangoapp/my_app/views.py
+++ djangoapp/my_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Candy
 
@@ -14,6 +14,14 @@ def candies_index(request):
     return render(request, 'my_app/candies_index.html', {'candies': candies})
 
 def candies_detail(request, candy_id):
-    candy = Candy.objects.get(id=candy_id)
-    return render(request, 'my_app/candies_detail.html', {'candy': candy})
+    candy = get_object_or_404(Candy, id=candy_id)
+    return render(request, 'my_app/candies/detail.html', {'candy': candy})
+
+def candies_create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        type = request.POST.get('type')
+        Candy.objects.create(name=name, type=type)
+        return redirect('candies_index')
+    return render(request, 'my_app/candies/create.html')
 
